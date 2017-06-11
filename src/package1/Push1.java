c152dba563fce779f5f7e9d63e6b9d3fa703c32b
Chandrakant Sao <Chandrakant.Sao@efi.com>
2017-06-11 14:01:33 +0530

    First Commit by Chandrakant Sao in JavaCode1

---
+++ src/package1/Push1.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class Push1 {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.println("Name\t:Chandrakant");
+			System.out.println("Message\t:Hello Chandrakant");
+			System.out.println("Class\t:Automation");
+			}
+	}
+
+
---
+++ src/package1/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author chandsao
+ *
+ */
+package package1;
